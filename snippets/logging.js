c8e6d0dc3ff40c76bdc5f99357e0ecf32a803263
S. Kurt Newman <skurtn@users.noreply.github.com>
2016-06-26 15:45:55 -0500

    Initial commit

---
+++ snippets/logging.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Test
+// @namespace    https://github.com/skurtn/TampermonkeyScripts
+// @version      1.0.0
+// @description  Logs information it finds on slashdot.org landing page
+// @author       S. Kurt Newman
+// @match        http://slashdot.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
