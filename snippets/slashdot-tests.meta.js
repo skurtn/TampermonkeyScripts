3d2c60b0c90ac2893488b03e774b62e09e7e76fe
S. Kurt Newman <skurtn@users.noreply.github.com>
2016-06-26 23:12:31 -0500

    Update slashdot-tests.meta.js

--- snippets/slashdot-tests.meta.js
+++ snippets/slashdot-tests.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @name     SlashdotTest
 // @version  1.0.2
-// @downloadUrl  https://raw.githubusercontent.com/skurtn/TampermonkeyScripts/master/snippets/slashdot-tests.js
+// @downloadURL  https://raw.githubusercontent.com/skurtn/TampermonkeyScripts/master/snippets/slashdot-tests.js
 // ==/UserScript==
