4eab9e5b2911aaa80d211414a1203edcfeb31006
Kurt Newman <kurt.newman@cpanel.net>
2016-07-01 19:02:20 -0500

    Added gateway-neverwinter.js

---
+++ snippets/gateway-neverwinter.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Gateway Neverwinter Tests
+// @namespace    https://github.com/skurtn/TampermonkeyScripts
+// @version      1.0.2
+// @description  Testing how to tinker with gateway.neverwinter.com
+// @author       S. Kurt Newman
+// @match        http*://gateway.playneverwinter.com/*
+// @run-at       document-end
+// @grant        GM_log
+// @noiframes
+// @downloadURL  https://github.com/skurtn/TampermonkeyScripts/blob/master/snippets/slashdot-tests.js
+// @require      https://code.jquery.com/jquery-3.0.0.min.js#sha256=6069398299730203aa434d1520ccf88ee8bf0aeee241aca18edbd85c78943432
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var Version = "1.0.2";
+    var Name = "Gatway Neverwinter Test";
+
+    VM_log( Name + "v" + Version + " started" );
+
+    VM_log( Name + "v" + Version + " ended" );
+})();
