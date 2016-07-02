3cf90ab482f893568794722fefffbf19048b0a98
Kurt Newman <kurt.newman@cpanel.net>
2016-07-01 19:08:10 -0500

    Added meta

--- snippets/gateway-neverwinter.meta.js
+++ snippets/gateway-neverwinter.meta.js
@@ -7,18 +7,8 @@
 // @match        http*://gateway.playneverwinter.com/*
 // @run-at       document-end
 // @grant        GM_log
+// @noiframes
 // @updateURL    https://raw.githubusercontent.com/skurtn/TampermonkeyScripts/master/snippets/gateway-neverwinter.meta.js
 // @downloadURL  https://raw.githubusercontent.com/skurtn/TampermonkeyScripts/master/snippets/gateway-neverwinter.js
 // @require      https://code.jquery.com/jquery-3.0.0.min.js#sha256=6069398299730203aa434d1520ccf88ee8bf0aeee241aca18edbd85c78943432
-// @noiframes
 // ==/UserScript==
-
-(function() {
-    'use strict';
-    var Version = "1.0.2";
-    var Name = "Gatway Neverwinter Test";
-
-    VM_log( Name + "v" + Version + " started" );
-
-    VM_log( Name + "v" + Version + " ended" );
-})();
